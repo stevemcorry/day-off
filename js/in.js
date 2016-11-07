6bd71c3f8fe0a45aaf279ce41f7d540607aa96a3
Stephen Corry <stevemcorry@gmail.com>
2016-11-07 16:54:04 -0700

    update inside greeting

--- js/in.js
+++ js/in.js
@@ -1,8 +1,8 @@
 App.controller('inCtrl',function($scope, mainService) {
-  $scope.insideAct = 'NETFLIX';
+  $scope.insideAct = 'Click For Ideas!';
   $scope.ideas = mainService.inIdeas();
   $scope.getAct = function() {
     $scope.insideAct = $scope.ideas[Math.floor(Math.random()*$scope.ideas.length)];
     };
-    
+
 });
